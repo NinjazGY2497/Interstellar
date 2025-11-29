7734925abb24aba668a1aeafdd19fe3f5e72a341
NinjazGY2497 <gyagau@gmail.com>
2025-11-28 21:18:34 -0500

    Undo

--- static/assets/js/tabcloak.js
+++ static/assets/js/tabcloak.js
@@ -1,15 +1,1 @@
-window.CLOAK_TEST = true;
-console.log("Tab cloak injected!");
 
-// Observe the whole page for changes
-const observer = new MutationObserver(() => {
-    // Force the tab title
-    document.title = "Home";
-
-    // Force the favicon to Google Classroom
-    let link = document.querySelector("link[rel~='icon']");
-    if (link) link.href = "https://ssl.gstatic.com/classroom/favicon.ico";
-});
-
-// Attach observer to document
-observer.observe(document, { subtree: true, childList: true });
